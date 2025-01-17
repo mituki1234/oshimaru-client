ac10d5da96dc051aaed71107c75c25a595483abb
柳瀬充希 <takyuudaisukimitu@gmail.com>
2025-01-17 10:53:10 +0900

    cloudflare pages用に1

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   server: {
     host: '127.0.0.1',
-    port: 5173,
   }
 })
